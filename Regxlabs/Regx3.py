c3fdf397a3f420ae2e70742b4e7016c4e087b52a
Santiago Criado <santi@Santiagos-MacBook-Air.local>
2023-01-21 16:25:45 -0500

    regx3 updated

--- Regxlabs/Regx3.py
+++ Regxlabs/Regx3.py
@@ -2,16 +2,16 @@ import sys; args = sys.argv[1:]
 idx = int(args[0])-50
 
 myRegexLst = [
-    r"/\w*(\w)\w*\1\w*/i",
-    r"/\w*(\w)\w*\1\w*\1\w*\1\w*/i", 
+    r"/(\w)*\w*\1\w*/i",
+    r"/(\w)*(\w*\1){3}\w*/i", 
     r"/^([01])[01]*\1$|^[01]$/",
     r"/\b(?=\w*cat)\w{6}\b/i",
     r"/\b(?=\w*bri)(?=\w*ing)\w{5,9}\b/i",
     r"/\b(?!\w*cat)\w{6}\b/i", 
-    r"/\b(?!\w*(\w)\w*\1)\w+\b/i", 
+    r"/\b(?!\w*(\w)\w*\1)\w+/i", 
     r"/^(?!.*10011)[01]*$/", 
     r"/\w*([aeoiu])(?!\1)[aeoiu]\w*/i",
-    r"/^(?!.*111)(?!.*101)[01]*$/"
+    r"/^(?!.*(111|101))[01]*$/"
 ] 
 if idx < len(myRegexLst):
   print(myRegexLst[idx])
